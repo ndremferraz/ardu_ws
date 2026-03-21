d15db0a3cc6c18c681a8cf1c677e93c0fdbff14b
ndremferraz <andreferraz@gwu.edu>
2026-03-21 15:35:22 -0400

    Added files

---
+++ src/mavros_examples/launch/mavros.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('my_drone_bringup'),
+        'config',
+        'new_apm_config.yaml'
+    )
+
+    plugins = os.path.join(
+        get_package_share_directory('mavros'),
+        'launch',
+        'apm_pluginlists.yaml'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='mavros',
+            executable='mavros_node',
+            namespace='mavros',
+            output='screen',
+            parameters=[
+                config,
+                plugins,
+                {
+                    'fcu_url': 'serial:///dev/ttyACM0:115200',
+                    'gcs_url': '',
+                    'tgt_system': 1,
+                    'tgt_component': 1,
+                    'fcu_protocol': 'v2.0',
+                }
+            ]
+        )
+    ])
---
+++ src/mavros_examples/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'mavros_examples'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='you',
+    maintainer_email='you@example.com',
+    description='MAVROS examples',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+            'flight_tasks_control = mavros_examples.flight_tasks_control:main',
+        ],
+    },
+)
