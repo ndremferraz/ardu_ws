950f1b2200935957bd74e1187f7c24532c016fb6
ndremferraz <andreferraz@gwu.edu>
2026-04-14 00:42:19 -0400

    Added cmd_vel_test.py

---
+++ src/mavros_examples/mavros_examples/send_cmd_vel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import TwistStamped
+
+class VelPub(Node):
+    def __init__(self):
+        super().__init__('vel_pub')
+
+        self.pub = self.create_publisher(
+            TwistStamped,
+            '/mavros/setpoint_velocity/cmd_vel',
+            10
+        )
+        
+        self.timer = self.create_timer(0.1, self.send_cmd)  # 10 Hz
+
+    def send_cmd(self):
+        msg = TwistStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'map'
+        msg.twist.linear.x = 0.2
+        msg.twist.linear.y = 0.0
+        msg.twist.linear.z = 0.0
+        msg.twist.angular.z = 0.0
+        self.pub.publish(msg)
+
+def main():
+    rclpy.init()
+    node = VelPub()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
